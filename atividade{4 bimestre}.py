fe5d08750bfb529d330e53c95689c08010bc3595
lara1710 <laraleticia1203@gmail.com>
2024-10-16 14:10:01 -0300

    dacl

---
+++ atividade{4 bimestre}.py
@@ -0,0 +1,16 @@
+# Crie duas funções:
+# - calcular_area_base, que recebe o raio de um círculo e retorna sua área.
+# - calcular_volume_cilindro, que utiliza a função calcular_area_base para calcular o volume de um cilindro dado o raio e a altura.
+
+import math
+
+def calcular_area_base(raio):
+    area = math.pi * (raio ** 2)
+    return area
+
+import math
+
+def calcular_area_base(raio):
+    area = math.pi * (raio ** 2)
+    return area
+
